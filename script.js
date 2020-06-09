b7ef9044af720dc00eb04ef60aa4880c3a24a26b
Ruslan Rustanovich ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2020-06-09 12:34:36 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@
 
 	// Creates solved puzzle
 	solve();
+// Listens for click on puzzle cells
+	puzzle.addEventListener('click', function(e){
+		if(state == 1){
+			// Enables sliding animation
+			puzzle.className = 'animate';
+			shiftCell(e.target);
